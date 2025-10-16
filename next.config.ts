c8fbf0353abe1fbc4488767bd4d25ba21fc7ab9e
WoongHwan-Kim <whkim@dareesoft.com>
2025-10-16 15:01:29 +0900

    update: dashboard & login page

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-    },
-  },
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
